0c966bc874f24787b78916e3c8f8ff6ba7c7f8b7
jasonfabriag <168501307+jasonfabriag@users.noreply.github.com>
2026-02-01 13:06:03 +0800

    Create test_app.py

---
+++ .github/workflows/test_app.py
@@ -0,0 +1,12 @@
+# test_app.py
+from app import add, subtract
+
+def test_add_positive_numbers():
+    # We "assert" (claim) that 2 + 3 should equal 5
+    assert add(2, 3) == 5
+
+def test_add_negative_numbers():
+    assert add(-1, -1) == -2
+
+def test_subtract():
+    assert subtract(10, 5) == 5
